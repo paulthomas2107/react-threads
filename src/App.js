6a15ce8519304d810fba3f4af02cf6347de136eb
Paul Thomas <paulthomas2107@gmail.com>
2023-07-12 19:58:32 +0100

    More

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Feed from './components/Feed';
 import PopUp from './components/PopUp';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const userId = 'acad37c7-7ec8-4e2a-921b-ed140ddc7274';
+
+  const getUser = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/users?user_uuid=${userId}`
+      );
+      const data = await response.json();
+      setUser(data[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  console.log(user)
+  
   return (
     <div className="app">
       <Nav></Nav>
