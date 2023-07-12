ea3cbb157b3e2940dade0050e190c49ce5fa3fdc
Paul Thomas <paulthomas2107@gmail.com>
2023-07-12 19:24:56 +0100

    More

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,11 @@
-import Thread from "./Thread"
+import Thread from './Thread';
 
 const Feed = () => {
-  return <div className="feed">
-    <Thread></Thread>
-  </div>;
+  return (
+    <div className="feed">
+      <Thread></Thread>
+    </div>
+  );
 };
 
 export default Feed;
