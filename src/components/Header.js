b202b6c661b15d8e4a4a0a0204cd9612cd939d87
Paul Thomas <paulthomas2107@gmail.com>
2023-07-12 17:27:04 +0100

    More

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,29 @@
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className="user-info-container">
+        <h1>Username</h1>
+        <p>
+          Handle <span className="threads-info">Threads.net</span>
+        </p>
+      </div>
+      <div className="img-container">
+        <img src="" alt="Profile avatar"></img>
+      </div>
+      <p>Bio</p>
+      <div className="sub-info-container">
+        <p className="sub-text">
+          X Followers • <a href="/">Link</a>
+        </p>
+      </div>
+      <button
+        className="primary"
+        onClick={() => navigator.clipboard.writeText('I am a URL')}
+      >
+        Share Profile
+      </button>
+    </header>
+  );
 };
 
 export default Header;
